8339d7efa7566766daa27345b95b6591bafd8381
Majoros <majoroscsilla@arcanumkft.com>
2021-10-09 10:21:17 +0200

    Add problematic jpeg names

--- data/arcanum_images.py
+++ data/arcanum_images.py
@@ -4,9 +4,16 @@ import cv2
 import tqdm
 import multiprocessing
 
+bad_file_paths = [
+    '/home/mnt/noah/_HungaricanaPic/jpg/gallery/fortepan/00185000/00186963.jpg',
+    '/home/mnt/noah/_HungaricanaPic/jpg/gallery/fortepan/00180000/00184027.jpg',
+    '/home/mnt/noah/_HungaricanaPic/jpg/gallery/fortepan/00180000/00184024.jpg',
+]
+
 def get_filenames(file_patterns):
-    image_paths = [file_path for file_pattern in file_patterns for file_path in glob(file_pattern, recursive=True)]
-    image_paths = _reduce_filenames(image_paths)
+    image_paths = [file_path for file_pattern in file_patterns for file_path in glob(file_pattern, recursive=True)
+                   if file_path not in bad_file_paths]
+    # image_paths = _reduce_filenames(image_paths)
     random.shuffle(image_paths)
     # 90% train images and 10% test images
     n_train_samples = int(len(image_paths) * 0.9)
@@ -30,4 +37,5 @@ def _reduce_filenames(image_paths):
         if check_img_path(img_path):
             reduced_paths.append(img_path)
     return reduced_paths
+
 
